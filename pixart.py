c1c97135f94d2bc1e474c4a0c021979576aa834f
kxkcad <kxkcad@rit.edu>
2021-09-26 14:55:25 +0400

    Added docString

--- pixart.py
+++ pixart.py
@@ -5,6 +5,7 @@ import turtle
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 def turtle_move(x,y):
+    "this function moved the turtle to a location provided by the caller"
     turtle.goto(x,y)
 
 
